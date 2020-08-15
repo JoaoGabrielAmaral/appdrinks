0c4d42a9c0dd40ab76c81567e84c9c65d004ded6
Joao Gabriel <joaogabrielamaral@outlook.com.br>
2020-08-15 16:42:02 -0300

    config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
-const App: React.FC = () => (
-  <View />
-);
+const App: React.FC = () => <View />;
 
 export default App;
